9e421de2f7d9d20ee16dbae0765d473050cb123b
didi-121 <edidier11@outlook.com>
2025-09-27 19:44:29 -0600

    serial com changed to I2C for motors

--- workspace/hardware/hardware/motor_node.py
+++ workspace/hardware/hardware/motor_node.py
@@ -1,59 +1,20 @@
-"""
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Float32MultiArray
-
-class MotorNode(Node):
-    def __init__(self):
-        super().__init__('motor_node')
-        self.subscription = self.create_subscription(Int32MultiArray, 'commands_for_dc_motors', self.cmd_vel_callback, 10)
-        self.publisher = self.create_publisher(Float32MultiArray, '/encoders', 10)
-        self.timer = self.create_timer(.01, self.cmd_enc_callback)
-        self.encoder_izq = 0
-        self.encoder_der = 0
-
-    def cmd_vel_callback(self, msg):
-        motor_izq, motor_der = msg.data
-        self.get_logger().info(f'Recibido: Izq: {motor_izq}, Der: {motor_der}')
-
-    def cmd_enc_callback(self):
-        self.encoder_izq = 1.5
-        self.encoder_der = 2.7
-
-        encoder_msg = Float32MultiArray()
-        encoder_msg.data = [self.encoder_izq, self.encoder_der]
-        self.publisher.publish(encoder_msg)
-
-def main():
-    rclpy.init()
-    node = MotorNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
-"""
-#!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Int32MultiArray
-from std_msgs.msg import Float32MultiArray
-import serial  
+import smbus2  
+import threading
 
 class MotorNode(Node):
     def __init__(self):
         super().__init__('motor_node')
 
-        
-        # try:
-        #     self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
-        #     self.get_logger().info("Conectado al puerto serial")
-        # except serial.SerialException:
-        #     self.get_logger().error("No se pudo abrir el puerto serial")
-        #     self.ser = None
+        # I2C Configuration
+        self.I2C_SLAVE_ADDRESS = 0x09  # Address of the Teensy
+        self.bus = smbus2.SMBus(1)
+        # Create a Lock object to manage access to the I2C bus
+        self.i2c_lock = threading.Lock()
 
         #recibir velocidades
         self.subscription = self.create_subscription(Float32MultiArray, 'commands_for_dc_motors', self.cmd_vel_callback, 10)
@@ -67,11 +28,34 @@ class MotorNode(Node):
         # variables de encoders
         self.encoder_izq = 0.0
         self.encoder_der = 0.0
+    
+    def motors(self, left, right):
+        if left < 0:
+            left = ((left) * -1) + 100
+        if right < 0:
+            right = ((right) * -1) + 100
+            
+        data = [(11, left), (12, right)]
+        for command, value in data:
+            self.send_data(command, value)
+    
+    def send_data(self,command, number):
+        try:
+            data_to_send = [command,
+                            (number >> 8) & 0xFF,  # High byte
+                            number & 0xFF]         # Low byte
+            
+            with self.i2c_lock:
+                self.bus.write_i2c_block_data(self.I2C_SLAVE_ADDRESS, command, data_to_send[1:])
+        except Exception as e:
+            att = 0
+                #print(f"Error sending data: {e}")
 
     def cmd_vel_callback(self, msg):
         #Recibe las velocidades y las envía al microcontrolador vía serial
         motor_der, motor_izq = msg.data
         self.get_logger().info(f'Recibido: Izq: {motor_izq}, Der: {motor_der}')
+        self.motors(int(motor_izq * 100 ), int(motor_der * 100))
 
         # # Enviar valores al microcontrolador
         # if self.ser:
@@ -107,3 +91,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+        
